8c40ee4ac49b2d56507d26d8c77cad3bcebf6fe9
memoselyk <memoselyk@gmail.com>
2024-10-22 19:41:53 -0600

    Add page title

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "🧠 Memo's Zetteln",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
